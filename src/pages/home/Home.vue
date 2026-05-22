7fbecca93d6b224d840c0cfd8cb1e72822b8cc79
rlopezse <rza.lopez.s@gmail.com>
2026-05-22 15:50:25 -0400

    fix: yml firebase

---
+++ src/pages/home/Home.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import posts from '../../data/json/posts.json'
+import PostItem from '../../components/post-item/PostItem.vue'
+</script>
+<template>
+  <div class="post">
+    <PostItem v-for="post in posts.data" :key="post.id" :post="post" />
+  </div>
+</template>
+<style scoped></style>
