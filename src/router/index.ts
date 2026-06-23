613cdc629804bf7b80f583f264baf7c4424adc0e
rlopezse <rza.lopez.s@gmail.com>
2026-06-23 09:30:41 -0400

    added: marcas por pagina

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import type { RouteLocationNormalized } from 'vue-router'
 import Home from '../pages/home/Home.vue'
 import Post from '../pages/post/Post.vue'
 import About from '../pages/about/About.vue'
@@ -9,7 +10,15 @@ const routes = [
   { path: '/:slug', component: Post },
 ]
 
-export default createRouter({
+const router = createRouter({
   history: createWebHistory(),
   routes,
 })
+
+router.afterEach((to: RouteLocationNormalized) => {
+  window.gtag?.('event', 'page_view', {
+    page_path: to.fullPath,
+  })
+})
+
+export default router
